df54b4c3d661aba053fbe17728eb3bb4d6928622
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2023-09-28 22:29:22 -0700

    Create an image

---
+++ src/main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+// Image dimensions
+int WIDTH = 1920;
+int HEIGHT = 1080;
+
+// Progress bar
+uint64_t PREVIOUS_UPDATE = 0;
+int PROGRESS_TICK_RATE = 10; /** in ms */
+
+uint64_t ms_time()
+{
+  struct timespec time;
+  clock_gettime(CLOCK_REALTIME, &time);
+  long seconds = time.tv_sec;
+  long nanoseconds = time.tv_nsec;
+  return (seconds * 1000) + (nanoseconds / 1e6);
+}
+
+void print_progress_bar(int status, int max, int length)
+{
+  double ratio = (double)status / (double)max;
+  int char_threshold = ratio * length;
+  char *bar = malloc(sizeof(char) * length);
+
+  for (int i = 0; i < sizeof(char) * length; i++)
+  {
+    if (i < char_threshold)
+    {
+      bar[i] = '#';
+    }
+    else
+    {
+      bar[i] = ' ';
+    }
+  }
+  int percentage = status == max ? 100 : ratio * 100;
+  uint64_t delta_time = (ms_time() - PREVIOUS_UPDATE);
+  bar[(sizeof(char) * length) - 1] = status == max ? '#' : ' ';
+  if (PREVIOUS_UPDATE == 0 || delta_time >= PROGRESS_TICK_RATE || status == max)
+  {
+    printf("\e[?25lProgress: [%s] (%d%%)\t%d/%d\r", bar, percentage, status, max);
+    PREVIOUS_UPDATE = ms_time();
+  }
+}
+
+int main()
+{
+  uint64_t start = ms_time();
+
+  printf("Creating %dx%d image to 'out.ppm'...\n", HEIGHT, WIDTH);
+  FILE *file = fopen("out.ppm", "wb");
+
+  // P3 header
+  fprintf(file, "P3\n%d %d\n255\n", WIDTH, HEIGHT);
+
+  // Generate the image top to bottom, with each row generated left to right
+  for (int i = 0; i < HEIGHT; i++)
+  {
+    for (int j = 0; j < WIDTH; j++)
+    {
+      int r = 255.999 * ((double)i / (WIDTH - 1));
+      int g = 255.999 * ((double)j / (HEIGHT - 1));
+      int b = (r + g) / 2;
+      fprintf(file, "%d %d %d\n", r, g, b);
+      print_progress_bar((i * WIDTH) + j + 1, WIDTH * HEIGHT, 20);
+    }
+  }
+  fclose(file);
+  printf("\nDone! (%" PRIu64 "ms)\n\e[?25h", ms_time() - start);
+
+  return 0;
+}
